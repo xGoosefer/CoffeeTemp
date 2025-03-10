3abe474ee9c5508b950c039ee931d1cbc9fdab72
Roberto Ramirez <robaram726@gmail.com>
2025-03-10 12:56:34 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * Coffee Event Handler Demo
+ * Roberto A Ramirez, 3/10/2025
+ * credits: 
+ * Modified and extended from example code at http://www.albahari.com/nutshell/E9-CH04.aspx
+ */
+
+namespace CoffeeTemp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Coffee Tempurature";
+        }
+    }
+}
---
+++ Simulation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeTemp
+{
+    internal class Simulation
+    {
+        Liquid liquid = new Liquid();
+    }
+}
